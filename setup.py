0e4d6f21ed2d2eb5e459d64ad3373075d04fd8af
kristian <kristianhudak777@gmail.com>
2024-06-08 18:47:03 +0200

    modified:   setup.py 	renamed:    src/library/__innit__.py -> src/libraries/__innit__.py 	renamed:    src/library/install_package.py -> src/libraries/install_package.py 	renamed:    src/library/my_image.py -> src/libraries/my_image.py 	renamed:    src/library/my_prime.py -> src/libraries/my_prime.py 	renamed:    src/library/my_time.py -> src/libraries/my_time.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='libraries',
     version='1.2.0',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
+    packages=find_packages(where='src/libraries'),
+    package_dir={'': 'src/libraries'},
     
 )
