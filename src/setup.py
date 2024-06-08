f6a50a2c4bd2328e3d3a473a8c90754d595ed2fe
kristian <kristianhudak777@gmail.com>
2024-06-08 17:53:36 +0200

    5.5 	modified:   src/setup.py

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='libraries',
-    version='1.0.4',
+    version='1.2.0',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     
