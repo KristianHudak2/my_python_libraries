7b5c6a3a7291db95beddcce7e56daa92f7002a00
kristian <kristianhudak777@gmail.com>
2024-06-20 15:05:56 +0200

    9.5.1 	new file:   __init__.py 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_python_libraries',
-    version='1.3.7',
+    name='libraries',
+    version='0.3.7',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     
